45bb7000d123c93ae89595611ce6ad2677da9c64
manuelmannuel <manuelmannuel@gmail.com>
2021-12-13 18:05:33 -0600

    add leo tweets

---
+++ bd/insertoTweet.go
@@ -0,0 +1,31 @@
+package bd
+
+import (
+	"context"
+	"time"
+
+	"github.com/devtimx/backTwit/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*InsertoTweet graba el Tweet en la DB*/
+func InsertoTweet(t models.GraboTweet) (string, bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("backTwit")
+	col := db.Collection("twitt")
+
+	registro := bson.M{
+		"userid":  t.UserID,
+		"mensaje": t.Mensaje,
+		"fecha":   t.Fecha,
+	}
+	result, err := col.InsertOne(ctx, registro)
+	if err != nil {
+		return "", false, err
+	}
+	objID, _ := result.InsertedID.(primitive.ObjectID)
+	return objID.String(), true, nil
+}
---
+++ routes/modificarPerfil.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/devtimx/backTwit/bd"
+	"github.com/devtimx/backTwit/models"
+)
+
+func ModificarPerfil(w http.ResponseWriter, r *http.Request) {
+	var t models.Usuario
+
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, "Datos incorrectos "+err.Error(), 400)
+		return
+	}
+	var status bool
+	status, err = bd.ModificoRegistro(t, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar modificar el registro. Reintente nuevamente "+err.Error(), 400)
+		return
+	}
+	if status == false {
+		http.Error(w, "No se ha logrado modificar el registro del usuario "+err.Error(), 400)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+
+}
